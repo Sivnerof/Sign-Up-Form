78569f706bb426c674902353a0a5300553aad546
Rogue Blue <rogueblueprint@protonmail.com>
2022-03-30 14:11:50 -0700

    Added JS warning to users about displaying PII in form due to GET method

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
-// Nothing Yet
+// Warning to user about actually inputing PII due to get method on form submission
+window.alert(
+    'Please note that this form is for demonstration purposes only.'
+    + '\n'
+    + 'Do not enter any personal information.'
+    + '\n'
+    + 'As it it will end up being displayed in the url of this page.'
+    + '\n'
+    + 'Have a nice day!');
